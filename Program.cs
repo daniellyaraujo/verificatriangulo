da1b198bca2da34e70562ffd05a7e432e7d4ca4f
Danielly Araujo <48495720+daniellyaraujo@users.noreply.github.com>
2019-07-12 13:55:05 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificaTriangulo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string entrada;
+            Triangulo objeto;
+            Console.WriteLine("Verifica se 3 valores formam um triangulo");
+
+            Console.Write("Lado A: ");
+            entrada = Console.ReadLine();
+            objeto = new Triangulo();
+            objeto.ladoA = Convert.ToInt32(entrada);
+
+            Console.Write("Lado B: ");
+            entrada = Console.ReadLine();
+            objeto.ladoB = Convert.ToInt32(entrada);
+
+            Console.Write("Lado C: ");
+            entrada = Console.ReadLine();
+            objeto.ladoC = Convert.ToInt32(entrada);
+
+            objeto.verificaTriangulo();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Triangulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificaTriangulo2
+{
+    class Triangulo
+    {
+        public int ladoA;
+        public int ladoB;
+        public int ladoC;
+
+        public void verificaTriangulo()
+        {
+            if ((ladoA<ladoB+ladoC)&&(ladoB<ladoA+ladoC)&&(ladoC<ladoA+ladoB))
+            {
+                Console.WriteLine("Forma um Triangulo!");
+            }
+            else
+            {
+                Console.WriteLine("Não forma um triangulo");
+            }
+        }
+    }
+}
